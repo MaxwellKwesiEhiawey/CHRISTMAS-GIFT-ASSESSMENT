f2efebe2c319377b30e001ef5387d5bbe6b23c37
MaxwellKwesiEhiawey <72410727+MaxwellKwesiEhiawey@users.noreply.github.com>
2020-12-21 20:33:57 +0000

    Update shop.js

--- Amalitech Cafeteria/controllers/shop.js
+++ Amalitech Cafeteria/controllers/shop.js
@@ -49,7 +49,7 @@ exports.getIndex = (req, res, next) => {
     .then(products => {
       res.render('shop/index', {
         prods: products,
-        pageTitle: 'Shop',
+        pageTitle: 'Amalitech Cafeteria',
         path: '/'
       });
     })
