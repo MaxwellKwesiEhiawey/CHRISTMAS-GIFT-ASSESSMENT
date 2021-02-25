1fd9b3a9f90e17fbd9355f21e03b84a415280ae7
MaxwellKwesiEhiawey <72410727+MaxwellKwesiEhiawey@users.noreply.github.com>
2021-02-25 14:36:06 +0000

    Logout fine

--- controllers/auth.js
+++ controllers/auth.js
@@ -104,6 +104,6 @@ exports.postSignup = (req, res, next) => {
 exports.postLogout = (req, res, next) => {
   req.session.destroy(err => {
     console.log(err);
-    res.redirect('/');
+    res.send('logout successfull!');
   });
 };
